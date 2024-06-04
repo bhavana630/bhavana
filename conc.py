5eed0057824e643702ba9182173cc8e96bf2a5c1
bhavana <bhavanakondapaneni630@gmail.com>
2024-06-04 10:46:04 +0530

    2nd try

---
+++ conc.py
@@ -0,0 +1,73 @@
+#concertention 
+str1="reddy"
+str2="bhavana"
+str3="kondapaneni"
+str4=str1+str2+str3
+print(str4)
+#repapation
+str = "kondapaneni"
+print(str*20)
+#membership
+bhavana = "btech final year"
+print("b" in bhavana)
+print("k" in bhavana)
+#methods
+#capitalize
+w ="kondapaneni"
+print(w.capitalize())
+#count
+s="she is good good"
+print(s.count("g"))
+#lower
+s="she is good good"
+print(s.lower())
+#upper
+x = "i am btech"
+print(x.upper())
+#startswith
+x = "i am btech"
+print(x.startswith("i"))
+#endwith
+x = "i am btech"
+print(x.endswith("i"))
+#find index
+x = "i am btech"
+print(x.index("i"))
+#isalnum
+x1 = "he is btech1"#space is their so it is false
+x2 = "good"
+print(x1.isalnum())
+print(x2.isalnum())
+#isalpha
+e1 = "nameisname"
+e2 = "nameisname1"
+print(e1.isalpha())
+print(e2.isalpha())
+#isnumberic
+w1="45673"
+w2="45673is"
+print(w1.isnumeric())
+print(w2.isnumeric())
+#islower
+e1="i am bhavana"
+e2="I am bhavana"
+print(e1.islower())
+print(e2.islower())
+#isupper
+e1="i am bhavana"
+e2="I AM BHAVANA"
+print(e1.isupper())
+print(e2.isupper())
+#isspace"
+e2=" "
+print(e2.isspace())
+#title
+r="bad"
+print(r.title())
+#split
+r="good is good"
+print(r.split("is"))
+#splitline
+print(r.splitlines("is"))
+#strip
+print(r.strip("good"))
